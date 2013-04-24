ee39c0d00c6ba619a97d1be8b272acb4228cf50f
thecopy <thecopy@gmail.com>
2013-04-24 14:30:19 +0200

    Initial nancy bare code

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace ControllWeb.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
