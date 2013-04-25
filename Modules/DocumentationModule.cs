530a72ce9ab4afd64b1f991762337220ab874462
thecopy <thecopy@gmail.com>
2013-04-25 19:54:19 +0200

    Added some documentation

---
+++ Modules/DocumentationModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace ControllWeb.Modules
+{
+    public class DocumentationModule : NancyModule
+    {
+        public DocumentationModule() : base("/Documentation/")
+        {
+            Get["/"] = _ => View["index.cshtml"];
+        }
+    }
+}
