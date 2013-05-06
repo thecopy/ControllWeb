4ac9c757d2dcf646b57f121a82449573f19ce99d
thecopy <thecopy@gmail.com>
2013-05-06 11:55:53 +0200

    Init commit with .gitignore and boilerplate

---
+++ IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace ControllWeb
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
